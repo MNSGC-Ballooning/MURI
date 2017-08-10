8fb2957eea6dfb7491a5bb09b37f2ada1e4e0a15
Simon P <simonpeterson76@gmail.com>
2017-08-10 12:00:14 -0500

    added dust sensor interrupt file

---
+++ Dust_Interrupt/Dust_Interrupt.ino
@@ -0,0 +1,24 @@
+/*
+ Simon Peterson, August 2017
+ Writing code to use the Telaire Smart Dust Sensor using interrupts to allow for multiple
+ sensors to be used.
+ pin 1 = GND
+ pin 2 = P2 signal out (large particles, 3~10 um)
+ pin 3 = +5V
+ pin 4 = P1 signal out (small particles, 1~2 um)
+ pin 5 no connection
+ */
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
