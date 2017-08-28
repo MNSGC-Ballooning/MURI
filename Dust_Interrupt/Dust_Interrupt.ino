58ef158b8fe2791cd28c4213cc658e7c8f760531
Simon P <simonpeterson76@gmail.com>
2017-08-28 12:24:09 -0500

    simple working rising and falling interrupt procedure

--- Dust_Interrupt/Dust_Interrupt.ino
+++ Dust_Interrupt/Dust_Interrupt.ino
@@ -14,51 +14,39 @@
 //declare {low,high} pins for the dustsensors
 dSen Sensors[2] = {{20,21},{9,10}};
 //the Low and High states of the dust sensor
-volatile byte state1;
-volatile byte state2;
 //pulse occupancy variables
 bool set = false;
-
-
+volatile bool change = false;
 
 
 void setup() {
-  pinMode(Sensors[0].getLow(), INPUT);
-  pinMode(Sensors[1].getLow(), INPUT);
+  Serial.begin(9600);
+  
   for(int i = 0; i<2;i++){
+    pinMode(Sensors[i].getLow(), INPUT);
+    pinMode(Sensors[i].getHigh(), INPUT);
     attachInterrupt(digitalPinToInterrupt(Sensors[i].getLow()), readSen, CHANGE);
     attachInterrupt(digitalPinToInterrupt(Sensors[i].getHigh()), readSen, CHANGE);
   }
 
 
 
-  Serial.begin(9600);
+  
   
 }
 
 void loop() {
-for(int i = 0; i<2; i++){
-  if(Sensors[i].changed){
-    Sensors[i].changed = false;
-    Serial.println("we are changing!");
+  if(change){
+    for(int i =0;i<2;i++){
+      Sensors[i].update();
+    }
+    change = false;
   }
-}
 }
 void readSen(){
-for(int i = 0; i < 2; i++){
-  Sensors[i].setLowState(digitalRead(Sensors[i].getLow()));
-  Sensors[i].setHighState(digitalRead(Sensors[i].getHigh()));
-  if(Sensors[i].getLowState()!=Sensors[i].getPrevLow())
-  {
-    Sensors[i].changed = true;
-    break;
-  }
-  if(Sensors[i].getHighState()!=Sensors[i].getPrevHigh()){
-    Sensors[i].changed = true;
-    break;
+  for(int i = 0; i<2; i++){
+    Sensors[i].checkStatus();
+    change = true;
   }
 }
-  
-}
-
 
