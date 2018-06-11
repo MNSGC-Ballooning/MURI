30ee7db9c192347e83b53f720b63753186e22215
garre <ailts008@umn.edu>
2018-06-11 10:56:57 -0500

    Added HASP payload folders with eagle schematics for the OPC Relay PCB

---
+++ HASP_Test/HASP_Test/HASP_Test.ino
@@ -0,0 +1,23 @@
+int setCoil = 2;
+int resetCoil = 3;
+void setup() {
+  pinMode(setCoil, OUTPUT);
+  pinMode(resetCoil,  OUTPUT);
+
+}
+
+void loop() {
+  delay(2500);
+  pulse(setCoil);
+  delay(5000);
+  pulse(resetCoil);
+  delay(5000);
+
+}
+
+void pulse(int coilPin){
+  digitalWrite(coilPin, HIGH);
+  delay(20);
+  digitalWrite(coilPin, LOW);
+}
+
